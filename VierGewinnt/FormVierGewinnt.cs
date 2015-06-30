c2d7b899eabd42b4bdb653091c454f6b852b936e
REGU_112 <user1@dv-schulen.de>
2015-06-30 09:30:22 +0200

    logik1

--- VierGewinnt/FormVierGewinnt.cs
+++ VierGewinnt/FormVierGewinnt.cs
@@ -12,10 +12,41 @@ namespace VierGewinnt
 {
     public partial class FormVierGewinnt : Form
     {
+        private PictureBox[,] picArray = new PictureBox[7,7];
         public FormVierGewinnt()
         {
             InitializeComponent();
+            InitializArray();
 
+        }
+
+        private void InitializArray()
+        {
+            //tableLayoutPanel1.SetColumn(pictureBox1, 0);
+            //tableLayoutPanel1.SetRow(pictureBox1, 1);
+            int z = 1;
+            int s = 0;
+            int r, c;
+           foreach(Control co in tableLayoutPanel1.Controls)
+           {
+               if(co.Name.Contains("pic"))
+               {
+                    PictureBox p = (PictureBox)co;
+                    p.BackColor = Color.Blue;
+                    z++;
+                    s++;
+                    r = z % 6 + 1;
+                    c = s % 7;
+                    picArray[r, c] = p;
+                    
+
+               }
+           }
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
