04b82aa1d586447ff1299178c727598d20fca5a2
regu_u12 <regu@we.de>
2016-06-16 14:47:50 +0200

    label entfernt

--- VierGewinnt/FormVierGewinnt.Designer.cs
+++ VierGewinnt/FormVierGewinnt.Designer.cs
@@ -39,7 +39,6 @@
             this.label1 = new System.Windows.Forms.Label();
             this.labelGelb = new System.Windows.Forms.Label();
             this.labelRot = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
             this.buttonNewGame = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
@@ -194,16 +193,6 @@
             this.labelRot.Visible = false;
             this.labelRot.MouseDown += new System.Windows.Forms.MouseEventHandler(this.labelRot_MouseDown);
             // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(93, 577);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(29, 31);
-            this.label8.TabIndex = 4;
-            this.label8.Text = "0";
-            // 
             // buttonNewGame
             // 
             this.buttonNewGame.Location = new System.Drawing.Point(90, 629);
@@ -222,7 +211,6 @@
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.ClientSize = new System.Drawing.Size(596, 692);
             this.Controls.Add(this.buttonNewGame);
-            this.Controls.Add(this.label8);
             this.Controls.Add(this.labelRot);
             this.Controls.Add(this.labelGelb);
             this.Controls.Add(this.tableLayoutPanel1);
@@ -247,7 +235,6 @@
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label labelGelb;
         private System.Windows.Forms.Label labelRot;
-        private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Button buttonNewGame;
     }
 }
